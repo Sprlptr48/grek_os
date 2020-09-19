70bda2d5203489ad7b1de112d49ba0935530c814
Sprlptr48 <egece2605@hotmail.com>
2020-09-19 11:08:00 +0300

    First Vga Buffer

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,22 @@
 #![no_main]
 
 
+static HELLO: &[u8] = b"How arr u?";
+
 #[no_mangle]
 pub extern "C" fn _start()  -> !{
-	loop{} //i�letim sistemi icin baslangic noktasi
+
+let vga_buffer = 0xb8000 as *mut u8;
+ 
+ for(a,&byte)in HELLO.iter().enumerate(){
+   unsafe{
+     *vga_buffer.offset(a as isize *2) = byte;
+     *vga_buffer.offset(a as isize *2 +1) = 0xd;
+   }
+  
+ }
+ 
+	loop{} //isletim sistemi icin baslangic noktasi
 }
 
 
