5ec555062a087c8278f0ff7d247543c75057e1b4
Sprlptr48 <egece2605@hotmail.com>
2020-09-19 11:16:06 +0300

    rlibc fix

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
 
 static HELLO: &[u8] = b"How arr u?";
 
@@ -15,7 +16,6 @@ let vga_buffer = 0xb8000 as *mut u8;
      *vga_buffer.offset(a as isize *2) = byte;
      *vga_buffer.offset(a as isize *2 +1) = 0xd;
    }
-  
  }
  
 	loop{} //isletim sistemi icin baslangic noktasi
